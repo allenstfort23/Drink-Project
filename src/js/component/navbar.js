2cc6660252f28d11ad3b3b19cd2b92fc9c2ee34c
Allen St Fort <allenstfort1@gmail.com>
2021-08-02 02:11:12 +0000

    Start of project

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,47 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1" href="#">
+					<i className="pl-3 fas fa-glass-martini-alt fa-3x" />
+				</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<div className="ml-auto pl-5">
+					<ul className="navbar-nav pl-5 mr-auto">
+						<li className="nav-item active">
+							<Link className="nav-link" aria-current="page" href="#">
+								Sign In
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" href="#">
+								About Us
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/demo" className="nav-link" href="#">
+								Contact
+							</Link>
+						</li>
+						<li className="list-inline-item">
+							<Link className="nav-link" href="#">
+								Account
+							</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,83 @@ import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="media sign-in picture">
+			<img
+				className="align-self-center mr-3"
+				src="https://miro.medium.com/max/1000/1*y-_byCrjIpAhg0SrHjIRTg.jpeg"
+				width="50%"
+			/>
+			<div className="form input mt-5 pl-5">
+				<form className="">
+					<div className="form-group">
+						<input
+							type="email"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+							placeholder="Email address"
+						/>
+						<small id="emailHelp" className="form-text text-muted">
+							We&apos;ll never share your email with anyone else.
+						</small>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className="form-control"
+							id="exampleInputPassword1"
+							placeholder="Password"
+						/>
+					</div>
+					<div className="form-group form-check">
+						<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+						<label className="form-check-label" htmlFor="exampleCheck1">
+							Check me out
+						</label>
+					</div>
+					<button type="submit" className="btn btn-lg">
+						Sign In
+					</button>
+				</form>
+			</div>
+		</div>
 	</div>
 );
+
+// width="500px"
+// height="500px"
+
+// <div className="sign-in picture float-left">
+// 				<div>
+// 					<img src="https://miro.medium.com/max/1000/1*y-_byCrjIpAhg0SrHjIRTg.jpeg" />
+// 				</div>
+// 			</div>
+// 			<div className="form input float-right">
+// 				<form>
+// 					<div className="form-group">
+// 						<label htmlFor="exampleInputEmail1">Email address</label>
+// 						<input
+// 							type="email"
+// 							className="form-control"
+// 							id="exampleInputEmail1"
+// 							aria-describedby="emailHelp"
+// 						/>
+// 						<small id="emailHelp" className="form-text text-muted">
+// 							We&apos;ll never share your email with anyone else.
+// 						</small>
+// 					</div>
+// 					<div className="form-group">
+// 						<label htmlFor="exampleInputPassword1">Password</label>
+// 						<input type="password" className="form-control" id="exampleInputPassword1" />
+// 					</div>
+// 					<div className="form-group form-check">
+// 						<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+// 						<label className="form-check-label" htmlFor="exampleCheck1">
+// 							Check me out
+// 						</label>
+// 					</div>
+// 					<button type="submit" className="btn btn-primary">
+// 						Submit
+// 					</button>
+// 				</form>
+// 			</div>
