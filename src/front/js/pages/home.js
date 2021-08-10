c58214ffcb09bcd6314ca97608cfca6397e0329b
Allen St Fort <allenstfort1@gmail.com>
2021-08-10 23:27:24 +0000

    New layout

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,17 +8,46 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<div className="media sign-in picture">
+				<img
+					className="align-self-center mr-3"
+					src="https://miro.medium.com/max/1000/1*y-_byCrjIpAhg0SrHjIRTg.jpeg"
+					width="50%"
+				/>
+				<div className="form input mt-5 pl-5">
+					<form className="">
+						<div className="form-group">
+							<input
+								type="email"
+								className="form-control"
+								id="exampleInputEmail1"
+								aria-describedby="emailHelp"
+								placeholder="Email address"
+							/>
+							<small id="emailHelp" className="form-text text-muted">
+								We&apos;ll never share your email with anyone else.
+							</small>
+						</div>
+						<div className="form-group">
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								placeholder="Password"
+							/>
+						</div>
+						<div className="form-group form-check">
+							<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+							<label className="form-check-label" htmlFor="exampleCheck1">
+								Check me out
+							</label>
+						</div>
+						<button type="submit" className="btn btn-lg">
+							Sign In
+						</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
